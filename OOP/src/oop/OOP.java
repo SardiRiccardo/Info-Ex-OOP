d675f353e55c3b8b89ee8f6b55803055903f3e52
SardiRiccardo <104832602+SardiRiccardo@users.noreply.github.com>
2022-05-12 19:39:54 +0200

    add exercise update 2

--- OOP/src/oop/OOP.java
+++ OOP/src/oop/OOP.java
@@ -8,7 +8,7 @@ public class OOP {
 
     public static void main(String[] args) {
 
-        Automobile auto1 = new Automobile(); //Quì creo l'auto numero 1 assegnandole modello targa ecc. e poi li stampo tutti 
+        Automobile auto1 = new Automobile();                                    //Quì creo l'auto numero 1 assegnandole modello targa ecc. e poi li stampo tutti 
 
         auto1.modello = "PapaMobile";
         auto1.targa = "PO420PE";
@@ -36,7 +36,7 @@ public class OOP {
         
         
         
-        Automobile auto2 = new Automobile(); //Quì creo l'auto numero 2 assegnandole modello targa ecc. e poi li stampo tutti 
+        Automobile auto2 = new Automobile();                                    //Quì creo l'auto numero 2 assegnandole modello targa ecc. e poi li stampo tutti 
 
         auto2.modello = "CrippaCottero";
         auto2.targa = "CT690MD";
@@ -54,19 +54,19 @@ public class OOP {
         
         
         
-        //Istanzio l'array fabbrica[] che racchiude 10 "Automobile" cioè 10 auto come la 1 e la 2
+                                                                                //Istanzio l'array fabbrica[] che racchiude 10 "Automobile" cioè 10 auto come la 1 e la 2
         
         Automobile fabbrica[] = new Automobile[10];
         
-        String arrayModello[] = new String[3]; //Quà dichiaro un array dei modelli che verranno assegnati casualmente
+        String arrayModello[] = new String[3];                                  //Quà dichiaro un array dei modelli che verranno assegnati casualmente
         arrayModello[0] = "A10";
         arrayModello[1] = "A20";
         arrayModello[2] = "A30";
         int randModello = 0;
         
-        int targa = 001; //Questo è il numero base da cui parte la targa: AA001AA, AA002AA, ..., AA010AA (perchè ho un array di 10)
+        int targa = 100;                                                        //Questo è il numero base da cui parte la targa: AA001AA, AA002AA, ..., AA010AA (perchè ho un array di 10)
         
-        String arrayColore[] = new String[7]; //Quà dichiaro i colori delle auto che verranno assegnati casualmente
+        String arrayColore[] = new String[7];                                   //Quà dichiaro i colori delle auto che verranno assegnati casualmente
         arrayColore[0] = "Rosso";
         arrayColore[1] = "Arancione";
         arrayColore[2] = "Giallo";
@@ -76,46 +76,45 @@ public class OOP {
         arrayColore[6] = "Viola";
         int randColore = 0;
         
-        int km = 0; //Essendo prodotte in fabbrica presumo che non abbiano un chilometraggio
+        int km = 0;                                                             //Essendo prodotte in fabbrica presumo che non abbiano un chilometraggio
         
-        int annoImmat = 2022; //Essendo appena uscite dalla fabbrica metto come anno di immatricolazione costante 2022
-        
-        
+        int annoImmat = 2022;                                                   //Essendo appena uscite dalla fabbrica metto come anno di immatricolazione costante 2022
         
         
         
+        for(int j = 0; j < 10; j++) {
+            fabbrica[j] = new Automobile();
+        }        
         
         
         for(int i = 0; i < 10; i++) {
             
             System.out.println("Automobile n." + i);
             
-            randModello = (int) (Math.random()*2); //Quà randomizzo la posizione dell'arrayModello per assegnare randomicamente il modello all'auto
+            randModello = (int) (Math.random()*2);                              //Quà randomizzo la posizione dell'arrayModello per assegnare randomicamente il modello all'auto
             fabbrica[i].modello = arrayModello[randModello]; 
             System.out.println("Modello: " + fabbrica[i].modello);
             
-            fabbrica[i].targa = "AA" + targa + "AA"; targa++; //Quà assegno la targa con le lettere uguali e aumentando i numeri di 1 ogni volta
+            fabbrica[i].targa = "AA" + targa + "AA"; targa++;                   //Quà assegno la targa con le lettere uguali e aumentando i numeri di 1 ogni volta
             System.out.println("Targa: " + fabbrica[i].targa);
             
-            int randCOlore = (int) (Math.random()*6); //Quà randomizzo la posizione dell'arrayColore per assegnare randomicamente il colore all'auto
+            int randCOlore = (int) (Math.random()*6);                           //Quà randomizzo la posizione dell'arrayColore per assegnare randomicamente il colore all'auto
             fabbrica[i].colore = arrayColore[randColore];
             System.out.println("Colore: " + fabbrica[i].colore);
             
-            fabbrica[i].KmFatti = km; //Quà assegno i chilometri
+            fabbrica[i].KmFatti = km;                                           //Quà assegno i chilometri
             System.out.println("Chilometri fatti: " + fabbrica[i].KmFatti);
             
-            fabbrica[i].annoImmatricolazione = annoImmat; //Quà assegno l'anno di immatricolazione
+            fabbrica[i].annoImmatricolazione = annoImmat;                       //Quà assegno l'anno di immatricolazione
             System.out.println("Anno di immatricolazione: " + fabbrica[i].annoImmatricolazione);
             
-            fabbrica[i].usata = false; //Essendo prodotte in fabbrica, le auto saranno nuove quindi "false" nell'usate
+            fabbrica[i].usata = false;                                          //Essendo prodotte in fabbrica, le auto saranno nuove quindi "false" nell'usate
             System.out.println("Usata: " + fabbrica[i].usata);
             
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
         }
-
-       /*Ogni volta che provo a far girare il programma mi dice: Exception in thread "main" java.lang.NullPointerException:
-        Cannot assign field "modello" because "fabbrica[i]" is null at oop.OOP.main(OOP.java:94). Ho provato a togliere quella parte e 
-        mi dice la stessa cosa per ogni proprietà dell'auto che assegno e non riesco a capire cosa significhi quell'errore :(  */
-
     }
-
 }
